da86da413d75fb30f006dcce5b3725c76868698c
maurizio schiavarelli <maurizioschiavarelli@gmail.com>
2024-02-22 19:45:13 +0100

    fine seconso esercizio

---
+++ main.js
@@ -0,0 +1,39 @@
+// Scrivi un programma che dato un numero, let num = 2, stampi la rispettiva tabellina corrispondente.
+
+
+// let num = 3
+// let num1 = 11
+
+// for (let i=1; i <= 10; i++){
+//     if((num * i) % num1 != 0){
+//         console.log(num * i)
+//     }
+// }
+
+
+
+
+
+//Scrivi un programma che stampi dei numeri da 1 a 20 ma solo quelli pari. Di tutti i numeri dispari, invece, calcola anche la media e stampala a schermo.
+
+
+
+// let media = 0;
+// let n = 20;
+// let contatore = 0;
+
+// for (let i = 1 ; i <= n; i++){
+    
+//     if( i % 2 == 0 ){
+//         console.log("I NUMERI PARI SONO " + i)
+//     } else {
+//         media = media + i;
+//         contatore ++;
+//     }
+// }
+
+// media = media/contatore;
+// console.log("la media dei nuemri dispari é " + media)
+
+
+
